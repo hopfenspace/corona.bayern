843a45b600e4d00d58d315d277751d3fc588ceb8
Jakob Löw <jakob@m4gnus.de>
2020-03-11 00:53:00 +0100

    scale the area of the circle according to the infected count, not the radius

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,7 @@ var mymap = L.map('coronamap').setView([48.834, 11.245], 8);
 L.tileLayer('https://a.tile.openstreetmap.de/{z}/{x}/{y}.png ', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, '
         + '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, '
+        + 'Kontakt: info@corona.bayern, '
         + 'Corona Zahlen vom 09.03.2020',
     maxZoom: 18,
 }).addTo(mymap);
@@ -12,7 +13,9 @@ function render(entries)
     for(var i = 0; i < entries.length; i++) {
         var entry = entries[i];
     
-        L.circle([entry.lat, entry.lng], {radius: entry.sick * 150, color: 'red', fillOpacity: 0.7})
+        var radius = Math.sqrt(entry.sick * 8e6 / Math.PI);
+        console.log(entry.name, entry.sick, radius);
+        L.circle([entry.lat, entry.lng], {radius: radius, color: 'red', fillOpacity: 0.7})
             .addTo(mymap)
             .bindPopup("<b>" + entry.name + "</b>" +
                 "<br />Infiziert: " + entry.sick +
