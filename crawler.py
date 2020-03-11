220d4628b6d5cab13db4ef8dea27b45eb29ff448
myOmikron <accounts@omikron.pw>
2020-03-11 13:33:07 +0100

    Refactored database scheme

--- crawler.py
+++ crawler.py
@@ -3,9 +3,9 @@ import json
 from datetime import datetime
 
 import requests
-from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime
+from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 
 
 def try_parsing_date(text):
@@ -31,15 +31,20 @@ def main():
     class Data(base):
         __tablename__ = 'data'
 
-        id = Column(Integer, primary_key=True)
-        title = Column(String(200))
+        id = Column(Integer, primary_key=True, autoincrement=True)
         infected = Column(Integer)
         deaths = Column(Integer)
         recovered = Column(Integer)
         date = Column(DateTime)
         source_1 = Column(String(200))
         source_2 = Column(String(200))
-        political = Column(String(200), unique=True)
+        location_id = Column(ForeignKey("Location.id"))
+        location = relationship("Location", back_populates="data")
+
+    class Location(base):
+        __tablename__ = "location"
+        id = Column(Integer, primary_key=True, autoincrement=True)
+        title = Column(String(200), unique=True)
         locality = Column(String(200))
         administrative_area_level_3 = Column(String(200))
         administrative_area_level_3_short = Column(String(5))
@@ -49,7 +54,8 @@ def main():
         country = Column(String(200))
         country_short = Column(String(5))
         lat = Column(Float)
-        lng = Column(Float)
+        long = Column(Float)
+        data = relationship("Data", back_populates="location")
 
     base.metadata.create_all(engine)
     session_builder = sessionmaker(bind=engine)
@@ -58,26 +64,25 @@ def main():
     data_list = []
 
     for item in data_db:
+        location = None if not item["location"] \
+            else Location(locality=item["location"]["address"].get("locality"),
+                          administrative_area_level_3=item["location"]["address"].get("administrative_area_level_3"),
+                          administrative_area_level_3_short=item["location"]["address"].get(
+                              "administrative_area_level_3_short"),
+                          administrative_area_level_2=item["location"]["address"].get("administrative_area_level_2"),
+                          administrative_area_level_1=item["location"]["address"].get("administrative_area_level_1"),
+                          administrative_area_level_1_short=item["location"]["address"].get(
+                              "administrative_area_level_1_short"),
+                          country=item["location"]["address"]["country"],
+                          country_short=item["location"]["address"]["country_short"],
+                          lat=item["location"]["lat"],
+                          long=item["location"]["lng"])
         d = Data(infected=int(item["infizierte"]) if item["infizierte"] != "" else 0,
                  deaths=int(item["todesfaelle"]) if item["todesfaelle"] != "" else 0,
                  recovered=int(item["geheilte"]) if item["geheilte"] != "" else 0,
-                 title=item["title"],
                  date=try_parsing_date(item["datum"]),
-                 source_1=item["quelle_1"], source_2=item["quelle_2"])
-        if item["location"]:
-            d.political = item["location"]["address"].get("political"),
-            d.locality = item["location"]["address"].get("locality"),
-            d.administrative_area_level_3 = item["location"]["address"].get("administrative_area_level_3"),
-            d.administrative_area_level_3_short = item["location"]["address"].get(
-                "administrative_area_level_3_short")
-            d.administrative_area_level_2 = item["location"]["address"].get("administrative_area_level_2"),
-            d.administrative_area_level_1 = item["location"]["address"].get("administrative_area_level_1"),
-            d.administrative_area_level_1_short = item["location"]["address"].get(
-                "administrative_area_level_1_short")
-            d.country = item["location"]["address"]["country"]
-            d.country_short = item["location"]["address"]["country_short"]
-            d.lat = item["location"]["lat"]
-            d.lng = item["location"]["lng"]
+                 source_1=item["quelle_1"], source_2=item["quelle_2"],
+                 location=location)
         data_list.append(d)
 
     session.add_all(data_list)
